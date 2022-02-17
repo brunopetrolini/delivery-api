98cad2a1bcfedb549ad5b749669b271aaf7686f8
Bruno Petrolini <brunopetrolini@hotmail.com>
2022-02-17 10:36:16 -0300

    add deliveryman authenticate route

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { AuthenticateClientController } from './modules/accounts/authenticate-client/authenticate-client-controller';
+import { AuthenticateDeliverymanController } from './modules/accounts/authenticate-deliveryman/authenticate-deliveryman-controller';
 import { CreateClientController } from './modules/clients/use-cases/create-client/create-client-controller';
 import { CreateDeliverymanController } from './modules/deliveryman/use-cases/create-deliveryman/create-deliveryman-controller';
 
@@ -10,6 +11,9 @@ routes.post('/clients/signin', authenticateClientController.handle);
 
 const createClientController = new CreateClientController();
 routes.post('/clients', createClientController.handle);
+
+const authenticateDeliverymanController = new AuthenticateDeliverymanController();
+routes.post('/deliveryman/signin', authenticateDeliverymanController.handle);
 
 const createDeliverymanController = new CreateDeliverymanController();
 routes.post('/deliveryman', createDeliverymanController.handle);
